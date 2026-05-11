144faeded48d81ff2f66f5f675cdacd5a36f1cae
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-05-12 03:16:19 +0530

    multi head attention

--- transformer.py
+++ transformer.py
@@ -8,7 +8,31 @@ class PositionalEncoding(nn.Module):
         super().__init__()
         self.dropout = nn.Dropout(dropout)
         pe = torch.zeros(max_len, d_model)         #(L, d)
-        pos = torch.arrange(max_len).unsqueeze(1).float()   #(d,1)
+        pos = torch.arange(max_len).unsqueeze(1).float()   #(d,1)
         div = torch.exp(
             torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
         )      
+        pe[:, 0::2] = torch.sin(pos * div)
+        pe[:, 1::2] = torch.cos(pos * div)
+
+        self.register_buffer("pe", pe.unsqueeze(0))
+
+        def forward(self, x: torch.Tensor) -> torch.Tensor:
+            self.dropout(x + pe[:, : x.size(1)])
+
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model: int, n_heads: int, dropout: float = 0.1): 
+        super().__init__()
+        assert d_model % n_heads == 0, "D_model needs to be divisible by n_head"
+
+        self.d_k = d_model // n_heads
+        self.n_heads = n_heads
+
+        self.W_q = nn.Linear(d_model, d_model)
+        self.W_k = nn.Linear(d_model, d_model)
+        self.W_v = nn.Linear(d_model, d_model)
+        self.W_o = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+        
